dd593b6c762421f79c64588aac4f7ddab9cd8107
Curtis Ostle <curtisostle@gmail.com>
2017-05-23 09:34:42 -0400

    finished page 2

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-console.log('connected')
-let aboutMe = $('#aboutme')
+let aboutMe = $('#aboutme');
+let uparrow = $('#uparrow');
+let downarrow = $('#downarrow');
+let page2 = $('#page2');
+
+
+uparrow.on('click', () => {
+	$.scrollTo(page1, 500)
+});
 
 aboutMe.on('click', () => {
-	$.scrollTo($('#page2'), 500)
+	$.scrollTo(page2, 500)
 });
