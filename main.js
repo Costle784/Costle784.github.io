2f034bbd28f3eb22d06424326c2c6ca926a2cf9d
Curtis Ostle <curtisostle@gmail.com>
2017-05-23 09:59:36 -0400

    added page 3

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-let aboutMe = $('#aboutme');
-let uparrow = $('#uparrow');
-let downarrow = $('#downarrow');
+//pages
+let page1 = $('#page1')
 let page2 = $('#page2');
-
-
-uparrow.on('click', () => {
-	$.scrollTo(page1, 500)
-});
+let page3 = $('#page3')
+//navigation
+let aboutMe = $('#aboutme');
+let projects = $('#projects');
+let uparrow1 = $('#uparrow1');
+let downarrow1 = $('#downarrow1');
+let uparrow2 = $('#uparrow2');
+let brandstatement= $('#brandstatement');
 
 aboutMe.on('click', () => {
-	$.scrollTo(page2, 500)
+	$.scrollTo(page2, 600)
+});
+projects.on('click', () => {
+	$.scrollTo(page3, 600)
+});
+uparrow1.on('click', () => {
+	$.scrollTo(page1, 600)
+});
+downarrow1.on('click', () => {
+	$.scrollTo(page3, 600)
+});
+uparrow2.on('click', () => {
+	$.scrollTo(page2, 600)
 });
