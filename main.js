97bc3755d12c3b22c88bacaca65b7e611560a751
Curtis Ostle <curtisostle@gmail.com>
2017-06-14 13:35:25 -0400

    finished nav and home page

--- main.js
+++ main.js
@@ -3,12 +3,21 @@ let page1 = $('#page1')
 let page2 = $('#page2');
 let page3 = $('#page3')
 //navigation
-let aboutMe = $('#aboutme');
+let home = $('#home')
+let about = $('#about');
 let projects = $('#projects');
 
 
-let brandstatement= $('#brandstatement');
 
-$("li").on('click', function(){
-	$(this).closest('li').addClass('active').siblings().removeClass('active')
-});
+about.on('click', () => {
+	$.scrollTo(page2, 300);
+	$(this.about).closest('li').addClass('active').siblings().removeClass('active')
+})
+home.on('click', () => {
+	$.scrollTo(page1, 300);
+	$(this.home).closest('li').addClass('active').siblings().removeClass('active')
+})
+projects.on('click', () => {
+	$.scrollTo(page3, 300);
+	$(this.projects).closest('li').addClass('active').siblings().removeClass('active')
+})
