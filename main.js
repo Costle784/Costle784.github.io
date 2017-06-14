e97b66a1a1013feafee4bd5dc53bc64dded11196
Curtis Ostle <curtisostle@gmail.com>
2017-06-14 11:18:47 -0400

    did nav on portfolio refactor

--- main.js
+++ main.js
@@ -5,15 +5,10 @@ let page3 = $('#page3')
 //navigation
 let aboutMe = $('#aboutme');
 let projects = $('#projects');
-let uparrow1 = $('#uparrow1');
-let downarrow1 = $('#downarrow1');
-let uparrow2 = $('#uparrow2');
+
 
 let brandstatement= $('#brandstatement');
 
-downarrow1.on('click', () => {
-	$.scrollTo(page2, 400)
-});
-uparrow2.on('click', () => {
-	$.scrollTo(page1, 400)
+$("li").on('click', function(){
+	$(this).closest('li').addClass('active').siblings().removeClass('active')
 });
