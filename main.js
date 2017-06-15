c9a43dca625eddae6a920f58588db0b967e8b02c
Curtis Ostle <curtisostle@gmail.com>
2017-06-15 00:27:51 -0400

    finished layout

--- main.js
+++ main.js
@@ -6,18 +6,31 @@ let page3 = $('#page3')
 let home = $('#home')
 let about = $('#about');
 let projects = $('#projects');
+let header = $('#header')
 
 
-
-about.on('click', () => {
-	$.scrollTo(page2, 300);
+about.on('click', (e) => {
+	$.scrollTo(page2, 200);
 	$(this.about).closest('li').addClass('active').siblings().removeClass('active')
 })
 home.on('click', () => {
-	$.scrollTo(page1, 300);
+	$.scrollTo(page1, 200);
 	$(this.home).closest('li').addClass('active').siblings().removeClass('active')
 })
 projects.on('click', () => {
-	$.scrollTo(page3, 300);
+	$.scrollTo(page3, 200);
 	$(this.projects).closest('li').addClass('active').siblings().removeClass('active')
+})
+
+$(window).scroll( () => {
+	let scroll = $(window).scrollTop();
+	if(scroll < 490) {
+		home.closest('li').addClass('active').siblings().removeClass('active')
+	}
+	else if(scroll > 490 && scroll < 840) {
+		about.closest('li').addClass('active').siblings().removeClass('active')
+	}
+	else {
+		projects.closest('li').addClass('active').siblings().removeClass('active')
+	}
 })
