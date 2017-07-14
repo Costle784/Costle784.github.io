4775d79a96dec6312b3a82239c7108fc3d1d4a2b
Curtis Ostle <curtisostle@gmail.com>
2017-07-14 12:49:40 -0400

    some portfolio edits

--- main.js
+++ main.js
@@ -11,15 +11,12 @@ let header = $('#header')
 
 about.on('click', (e) => {
 	$.scrollTo(page2, 200);
-	$(this.about).closest('li').addClass('active').siblings().removeClass('active')
 })
 home.on('click', () => {
 	$.scrollTo(page1, 200);
-	$(this.home).closest('li').addClass('active').siblings().removeClass('active')
 })
 projects.on('click', () => {
 	$.scrollTo(page3, 200);
-	$(this.projects).closest('li').addClass('active').siblings().removeClass('active')
 })
 
 $(window).scroll( () => {
@@ -27,7 +24,7 @@ $(window).scroll( () => {
 	if(scroll < 490) {
 		home.closest('li').addClass('active').siblings().removeClass('active')
 	}
-	else if(scroll > 490 && scroll < 840) {
+	else if(scroll > 490 && scroll < 1200) {
 		about.closest('li').addClass('active').siblings().removeClass('active')
 	}
 	else {
